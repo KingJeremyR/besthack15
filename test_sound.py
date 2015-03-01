efbe721ae2e0b49c55354f6ba8ff96561c17e787
Andy Barron <AndrewLBarron@gmail.com>
2015-02-28 19:20:09 -0500

    Polling, sound, merges Justin's code

--- test_sound.py
+++ test_sound.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import pyglet # installed via pip3
-import time # Python built-in
-import sfx # sfx.py
+import time   # Python built-in
+import sfx    # sfx.py
 sfx.play_file("assets/sfx/button-3.wav") # full path
 time.sleep(1)
 sfx.play("zoom.wav") # just filename (if in assets/sfx)
